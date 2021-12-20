aea3fce0d3dca731c06e626e2929de9360b12b1f
notequal21 <kalorigen@gmail.com>
2021-12-20 11:19:18 +0300

    added defaultState to table-reducer

--- src/state/table-reducer.js
+++ src/state/table-reducer.js
@@ -1,11 +1,24 @@
 
 const defaultState = {
-
+    id: 0,
+    Fullname: 'Name Surname',
+    Days: [
+        {
+            Date: "2021-01-01",
+            End: "20-00",
+            Start: "12-00",
+        },
+        {
+            Date: "2021-01-02",
+            End: "19-00",
+            Start: "13-00",
+        },
+    ]
 }
 
 const tableReducer = (state = defaultState, action) => {
     switch (action.type) {
-
+        
 
         default: 
             return state
